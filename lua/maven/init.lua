8e8d2870b4eb0d800eeb46f75503e66a8a1dfa03
glaulher <glaulher.developer@gmail.com>
2024-10-05 02:10:29 -0300

    feat:test new configurations2

--- lua/maven/init.lua
+++ lua/maven/init.lua
@@ -100,7 +100,7 @@ function maven.execute_command(command)
     return
   end
 
-  if not has_build_file(cwd) and not command.cmd[1] == "create" then
+  if not has_build_file(cwd) then
     vim.notify("no pom.xml file found under " .. cwd, vim.log.levels.ERROR)
     return
   end
