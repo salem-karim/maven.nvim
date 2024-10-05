8bebb1225b89ab50c75a387c4ff8f39fcc2089dc
glaulher <glaulher.developer@gmail.com>
2024-10-05 02:32:26 -0300

    feat:test new configurations

--- lua/maven/init.lua
+++ lua/maven/init.lua
@@ -102,7 +102,9 @@ function maven.execute_command(command)
   -- end
 
   if command.cmd[1] ~= "archetype:generate" then
+    vim.notify("command.cmd[1]: " .. tostring(command.cmd[1]), vim.log.levels.INFO)
     if not has_build_file(cwd) then
+      vim.notify("command.cmd[1]: " .. tostring(command.cmd[1]), vim.log.levels.INFO)
       vim.notify("no pom.xml file found under " .. cwd, vim.log.levels.ERROR)
       return
     end
