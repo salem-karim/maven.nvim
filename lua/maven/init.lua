4e604695209ac83de9f7f2adc1030fd89052421b
glaulher <glaulher.developer@gmail.com>
2024-10-06 10:20:09 -0300

    feat:test new configurations

--- lua/maven/init.lua
+++ lua/maven/init.lua
@@ -89,12 +89,12 @@ function maven.add_dependency_to_pom()
   -- Open Maven Central for OS
   -- local os_name = vim.loop.os_uname().sysname
 
-  local os_name = vim.loop.os_uname().sysname
+  local os_name, err = pcall(function()
+    return vim.loop.os_uname().sysname
+  end)
 
   if not os_name then
     vim.notify("Error getting OS name: " .. err, vim.log.levels.ERROR)
-  else
-    print("Operating System: " .. os_name)
   end
 
   if os_name == "Linux" then
