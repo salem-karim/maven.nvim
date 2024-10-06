6f0f74dce949f81340f3a680a2dd486cf0c0dbac
glaulher <glaulher.developer@gmail.com>
2024-10-06 19:30:42 -0300

    feat:test new configurations

--- lua/maven/init.lua
+++ lua/maven/init.lua
@@ -220,8 +220,8 @@ function maven.add_dependency_to_pom()
       if insert_index then
         -- Formata a dependência com indentação apropriada (4 espaços por nível)
         local formatted_dependency = string.format(
-          "    %s\n    ",
-          dependency:gsub("\n", "\n    ") -- Adiciona indentação a cada linha da dependência
+          "    %s",
+          dependency:gsub("  \n", "\n    ") -- Adiciona indentação a cada linha da dependência
         )
         table.insert(lines, insert_index, formatted_dependency)
       else
