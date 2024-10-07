3f83eb14b9a8d0101727b0db8f3e782ebc1155d6
glaulher <glaulher.developer@gmail.com>
2024-10-06 21:29:11 -0300

    feat:test new configurations

--- lua/maven/init.lua
+++ lua/maven/init.lua
@@ -144,7 +144,7 @@ function maven.add_dependency_to_pom()
   -- Function to remove the instruction message
   local function remove_instruction()
     local first_line = vim.api.nvim_buf_get_lines(buf, 0, 1, false)[1]
-    if first_line == "Cole a dependência aqui e pressione enter para adicionar ao pom.xml." then
+    if first_line == "Paste the dependency here and press enter to add it to the pom.xml." then
       -- Removes the first line from the buffer
       vim.api.nvim_buf_set_lines(buf, 0, 1, false, {})
       -- Repositions the cursor to the line above the next line
