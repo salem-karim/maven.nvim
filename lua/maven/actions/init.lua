a258161e55acc3717474213ef55901174ddc5330
glaulher <glaulher.developer@gmail.com>
2024-10-12 23:59:06 -0300

    feat:refactor

--- lua/maven/actions/init.lua
+++ lua/maven/actions/init.lua
@@ -1,6 +1,6 @@
 local M = {}
 
 M.create_project = require("maven.actions.create_project").create_project
-M.add_repository = require("maven.actions.add_repository_to_pom").add_repository_to_pom
+M.add_repository_to_pom = require("maven.actions.add_repository_to_pom").add_repository_to_pom
 
 return M
