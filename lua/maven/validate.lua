0b9988b75ad01dd75c712dd50d11d4ab8e68cfe3
glaulher <glaulher.developer@gmail.com>
2024-10-12 21:53:06 -0300

    feat:refactor

--- lua/maven/validate.lua
+++ lua/maven/validate.lua
@@ -1,9 +1,16 @@
 local M = {}
 
+-- Função para obter o diretório de trabalho atual
+local function get_cwd()
+  return require("maven.config").options.cwd or vim.fn.getcwd()
+end
+
+-- Verifica se existe um arquivo pom.xml no diretório
 local function has_build_file(cwd)
   return vim.fn.findfile("pom.xml", cwd) ~= ""
 end
 
+-- Verifica se uma tag com conteúdo específico está presente no pom.xml
 local function has_required_tag_in_pom(cwd, tag, content)
   local pom_file = cwd .. "/pom.xml"
 
@@ -21,11 +28,14 @@ local function has_required_tag_in_pom(cwd, tag, content)
 end
 
 -- Função de validação para verificar as condições antes de executar o comando
-function M.validate(cmd, cwd)
+function M.validate(cmd)
+  local cwd = get_cwd() -- Obtenha o diretório de trabalho atual uma única vez
+
   if type(cmd.cmd) ~= "table" or not cmd.cmd[1] then
     return false, "Invalid command structure."
   end
 
+  -- Verifica se há um pom.xml ou se é um comando de criação
   if cmd.cmd[1] ~= "create" and cmd.cmd[1] ~= "archetype:generate" and not has_build_file(cwd) then
     return false, "No pom.xml file found under " .. cwd
   end
@@ -34,7 +44,7 @@ function M.validate(cmd, cwd)
     -- Se houver um arquivo `pom.xml` no diretório
     if has_build_file(cwd) then
       if has_required_tag_in_pom(cwd, "packaging", "pom") then
-        return true, "Required tag found in pom.xml. Proceeding with Maven project creation."
+        return true, "Required tag found in pom.xml. Proceeding with Maven multi module project creation."
       else
         return false,
           "There is a pom.xml file indicating that there is already a Maven project in the directory: " .. cwd
@@ -48,4 +58,5 @@ function M.validate(cmd, cwd)
   -- Caso contrário, permite a execução do comando Maven normalmente
   return true, "Command is valid and can be executed."
 end
+
 return M
