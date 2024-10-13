81412a46a9dd47dfe51a33353307255807d59876
glaulher <glaulher.developer@gmail.com>
2024-10-13 02:03:15 -0300

    feat:refactor

--- lua/maven/validate.lua
+++ lua/maven/validate.lua
@@ -22,8 +22,7 @@ end
 -- validation to check conditions before executing the command
 function M.validate(cmd, cwd)
   if type(cmd.cmd) ~= "table" or not cmd.cmd[1] then
-    --  return false, "Invalid command structure."
-    print(cmd.cmd[1])
+    return false, "Invalid command structure."
   end
 
   if cmd.cmd[1] == "add-dependency" then
